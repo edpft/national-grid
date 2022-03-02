072d677002562ce4821341e9e017baf512bab4d0
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2022-03-02 14:05:50 +0000

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+            },
+        ],
+
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".wasm"]
+    },
+    mode: "development"
+};
